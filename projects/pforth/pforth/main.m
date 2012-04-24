64cf7c2e4e3a763a2a71c1a5ba451c302498cbd4
Nick Main <david.nick.main@gmail.com>
2012-04-23 22:10:39 -0700

    Xcode project for building command line pforth

---
+++ projects/pforth/pforth/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  pforth
+//
+//  Created by Nick Main on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#define PF_SUPPORT_FP
+#include "pf_all.h"
+#import <Foundation/Foundation.h>
+
+int mainX(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
